2d217e66c8e599575f6466859aff643674a0672c
tu4mo <tu4mo@users.noreply.github.com>
2016-02-11 17:33:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# HashScroll.js
+
+## Usage
+
+Include hashscroll.min.js in your page:
+
+```html
+<script src="hashscroll.min.js"></script>
+```
+
+Add `hashscroll` class to links:
+
+```html
+<a href="#section-1" class="hashscroll">Scroll to section 1</a>
+```
+
+Add id's to elements:
+
+```html
+<section id="section-1"></section>
+```
---
+++ hashscroll.js
@@ -0,0 +1,66 @@
+/**
+ * HashScroll.js v0.1.0
+ * https://github.com/tu4mo/hashscroll.js
+ *
+ * Copyright © 2016 tu4mo
+ */
+
+(function(window, document) {
+
+	'use strict';
+
+	function init() {
+		addEventListeners();
+	}
+
+	function addEventListeners() {
+		var elements = document.querySelectorAll('.hashscroll');
+
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].addEventListener('click', elementClicked);
+		}
+	}
+
+	function elementClicked(event) {
+		event.preventDefault();
+
+		var element = document.getElementById(this.getAttribute('href').substring(1));
+
+		scrollTo(element.getBoundingClientRect().top, 500);
+	}
+
+	function scrollTo(to, duration) {
+		var start = document.body.scrollTop || document.documentElement.scrollTop;
+		var change = to - start;
+		var increment = 20;
+
+		var animateScroll = function(elapsedTime) {
+			elapsedTime += increment;
+			var position = easeInOut(elapsedTime, start, change, duration);
+			document.body.scrollTop = document.documentElement.scrollTop = position;
+
+			if (elapsedTime < duration) {
+				setTimeout(function() {
+					animateScroll(elapsedTime);
+				}, increment);
+			}
+		};
+
+		animateScroll(0);
+	}
+
+	function easeInOut(currentTime, start, change, duration) {
+		currentTime /= duration / 2;
+
+		if (currentTime < 1) {
+			return change / 2 * currentTime * currentTime + start;
+		}
+
+		currentTime -= 1;
+
+		return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
+	}
+
+	init();
+
+})(window, document);
